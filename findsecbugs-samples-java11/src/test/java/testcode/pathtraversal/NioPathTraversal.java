ebff4aae68c1b1e601727819142088a035ed3bd7
Sebastian Lövdahl <slovdahl@hibox.fi>
2025-12-11 19:07:11 +0200

    fix spaces

--- findsecbugs-samples-java11/src/test/java/testcode/pathtraversal/NioPathTraversal.java
+++ findsecbugs-samples-java11/src/test/java/testcode/pathtraversal/NioPathTraversal.java
@@ -9,8 +9,8 @@ public class NioPathTraversal {
 
     public void loadFile(String path) throws URISyntaxException {
         Paths.get(path);
-        Paths.get(path,"foo");
-        Paths.get(path,"foo", "bar");
+        Paths.get(path, "foo");
+        Paths.get(path, "foo", "bar");
         Paths.get("foo", path);
         Paths.get("foo", "bar", path);
 
@@ -18,14 +18,14 @@ public class NioPathTraversal {
         Paths.get(new URI("file", path, "foo"));
 
         Paths.get("foo");
-        Paths.get("foo","bar");
-        Paths.get("foo","bar", "allsafe");
+        Paths.get("foo", "bar");
+        Paths.get("foo", "bar", "allsafe");
         Paths.get(new URI("file", "foo", null));
         Paths.get(new URI("file", "foo", "bar"));
 
         Path.of(path);
-        Path.of(path,"foo");
-        Path.of(path,"foo", "bar");
+        Path.of(path, "foo");
+        Path.of(path, "foo", "bar");
         Path.of("foo", path);
         Path.of("foo", "bar", path);
 
@@ -33,8 +33,8 @@ public class NioPathTraversal {
         Path.of(new URI("file", path, "foo"));
 
         Path.of("foo");
-        Path.of("foo","bar");
-        Path.of("foo","bar", "allsafe");
+        Path.of("foo", "bar");
+        Path.of("foo", "bar", "allsafe");
         Path.of(new URI("file", "foo", null));
         Path.of(new URI("file", "foo", "bar"));
     }
